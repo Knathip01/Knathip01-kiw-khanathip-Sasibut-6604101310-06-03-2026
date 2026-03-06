529d7bbbf49fa3b13a13646ca6e60384e153eedb
papasorn1605 <papasorn1605@gmail.com>
2026-03-06 13:49:41 +0700

    feat: add user data to seed script

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -24,6 +24,10 @@ async function main() {
             title: 'Cloud Deployment',
             description: 'Deploy Backend to Render and Frontend to Netlify',
         },
+        {
+            title: 'คณาธิป ศะศิบุตร์',
+            description: 'รหัสนักศึกษา: 6604101310',
+        },
     ];
 
     for (const t of initialTasks) {
